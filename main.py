b79d3a45f89ec152f10b630e1ac31845e8bbbeb2
PornHubNezin <77491318+PornHubNezin@users.noreply.github.com>
2021-08-12 13:21:25 +0300

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-driver.execute_script("document.getElementById('idd').innerHTML = '200';");
+t =  driver.find_element_by_class_name("name");
+        driver.execute_script("arguments[0].setAttribute('style', 'transition: transform 2500ms bla bla bla')", element);
